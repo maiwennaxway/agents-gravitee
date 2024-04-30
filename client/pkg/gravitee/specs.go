02b221c465c42cc7d73d52c5728d7cae5863121a
Maiwenn <msimon@axway.com>
2024-04-30 16:46:55 +0200

    Ajout bearer token

--- client/pkg/gravitee/specs.go
+++ client/pkg/gravitee/specs.go
@@ -11,6 +11,7 @@ func (a *GraviteeClient) GetSpecFile(specPath string) ([]byte, error) {
 	// Get the spec file
 	response, err := a.newRequest(http.MethodGet, fmt.Sprintf("%s%s", a.orgURL, specPath),
 		WithDefaultHeaders(),
+		WithHeader("Authorization", "Bearer 8f734df7-a350-44c3-b34d-f7a350c4c37a"),
 	).Execute()
 
 	if err != nil {
@@ -37,6 +38,7 @@ func (a *GraviteeClient) GetAllSpecs() ([]SpecDetails, error) {
 	// Get the spec file
 	response, err := a.newRequest(http.MethodGet, fmt.Sprintf("%s/environments/%s/apis", a.orgURL, a.EnvId),
 		WithDefaultHeaders(),
+		WithHeader("Authorization", "Bearer 8f734df7-a350-44c3-b34d-f7a350c4c37a"),
 	).Execute()
 
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/maiwennaxway/agents-gravitee
 
 go 1.21.6
 
-require github.com/maiwennaxway/agents-gravitee/client v0.0.0-20240430135129-a2e6409d2d32// indirect
+require github.com/maiwennaxway/agents-gravitee/client v0.0.0-20240430140445-eb349a641b6b// indirect
