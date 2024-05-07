d2ca77881c7fbcce996b5c149f4d95675add2b14
Maiwenn <msimon@axway.com>
2024-05-07 15:55:54 +0200

    test

--- client/pkg/gravitee/apicalls.go
+++ client/pkg/gravitee/apicalls.go
@@ -3,6 +3,7 @@ package gravitee
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/maiwennaxway/agents-gravitee/client/pkg/gravitee/models"
@@ -26,23 +27,21 @@ import (
 // GetListAPIs - get the list of APIs
 func (a *GraviteeClient) GetApis() (apis Apis, error error) {
 	//
-	req, err := a.newRequest(http.MethodGet, fmt.Sprintf("%s/environments/%s/apis", a.cfg.Auth.URL, a.cfg.EnvName),
-		//WithDefaultHeaders(),
-		WithHeader("Content-Type", "application/json"),
-		//WithHeader("Accept", "application/json"),
-		WithToken(a.GetConfig().Auth.GetToken()),
-	).Execute()
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/environments/%s/apis", a.GetConfig().Auth.GetURL(), a.cfg.EnvName), nil)
+	req.Header.Add("Accept", "application/json")
+	//req.Header.Add("Authorization", "Basic YWRtaW46YWRtaW4=")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.GetConfig().Auth.GetToken()))
 
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
-
-	if req.Code != http.StatusOK {
-		return nil, fmt.Errorf("received an unexpected response code %d from Gravitee when retrieving the app", req.Code)
-	}
-
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	fmt.Println(res)
+	fmt.Println(string(body))
 	apis = Apis{}
-	json.Unmarshal(req.Body, &apis)
+	json.Unmarshal(body, &apis)
 
 	return apis, nil
 
