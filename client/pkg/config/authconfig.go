61fa652a5d049ab916946b5e868c0ef72962d3d8
maiwennaxway <149514032+maiwennaxway@users.noreply.github.com>
2025-05-12 14:44:10 +0200

    Update authconfig.go

--- client/pkg/config/authconfig.go
+++ client/pkg/config/authconfig.go
@@ -2,7 +2,25 @@ package config
 
 // AuthConfig - represents the config for gateway
 type AuthConfig struct {
-	Token string `config:"token"`
+	URL      string `config:"url"`
+	Username string `config:"username"`
+	Password string `config:"password"`
+	Token    string `config:"token"`
+}
+
+// GetURL - Returns the gravitee username
+func (a *AuthConfig) GetURL() string {
+	return a.URL
+}
+
+// GetUsername - Returns the gravitee username
+func (a *AuthConfig) GetUsername() string {
+	return a.Username
+}
+
+// GetPassword - Returns the gravitee password
+func (a *AuthConfig) GetPassword() string {
+	return a.Password
 }
 
 func (a *AuthConfig) GetToken() string {
